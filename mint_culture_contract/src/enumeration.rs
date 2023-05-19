42be1659242e891c64a6cb3064ec4425f6c81d3f
psychehose <psychehose@gmail.com>
2023-05-19 23:28:13 +0900

    업데이트 컨트랙트: nft_tokens_for_owner 구현

--- mint_culture_contract/src/enumeration.rs
+++ mint_culture_contract/src/enumeration.rs
@@ -36,9 +36,25 @@ impl Contract {
         from_index: Option<U128>,
         limit: Option<u64>,
     ) -> Vec<JsonToken> {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+
+        // 전달된 소유자에 대한 토큰 세트 가져오기
+        let tokens_for_owner_set = self.tokens_per_owner.get(&account_id);
+        let tokens = if let Some(tokens_for_owner_set) = tokens_for_owner_set {
+            tokens_for_owner_set
+        } else {
+            //if there is no set of tokens, we'll simply return an empty vector.
+            return vec![];
+        };
+
+        let start = u128::from(from_index.unwrap_or(U128(0)));
+        tokens.iter()
+            //skip to the index we specified in the start variable
+            .skip(start as usize)
+            //take the first "limit" elements in the vector. If we didn't specify a limit, use 50
+            .take(limit.unwrap_or(50) as usize)
+            //we'll map the token IDs which are strings into Json Tokens
+            .map(|token_id| self.nft_token(token_id.clone()).unwrap())
+            //since we turned the keys into an iterator, we need to turn it back into a vector to return
+            .collect()
     }
 }
